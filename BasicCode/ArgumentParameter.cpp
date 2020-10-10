674fbd2c7112e97c0f230df7917bf58e1b481e6e
SCUPRISCILLA <941705864@qq.com>
2020-10-10 13:14:31 +0800

    增加了数组这种地址传递

--- BasicCode/ArgumentParameter.cpp
+++ BasicCode/ArgumentParameter.cpp
@@ -2,21 +2,34 @@
 
 using namespace std;
 
-void swapnum(int num1, int num2);
+void SwapNum(int num1, int num2);
+void SwapArrayNum(int arr[]);
+
 int main()
 {
     int a = 1;
     int b = 2;
-    swapnum(a, b);
+    SwapNum(a, b);
     cout << a << " " << b << endl;
+
+    int arr[2] = {1, 2};
+    SwapArrayNum(arr);
+    cout << arr[0] << "  " << arr[1];
     return 0;
 }
 
-void swapnum(int num1, int num2)
+void SwapNum(int num1, int num2)  // 值传递，实参复制一份给形参，实参值不会改变
 {
     int temp = num1;
     num1 = num2;
     num2 = temp;
     cout << "num1 = " << num1 << endl;
     cout << "num2 = " << num2 << endl;
+}
+
+void SwapArrayNum(int arr[])  // 数组都是地址传递，因为数组名就是地址，实参会改变
+{
+    int temp = arr[0];
+    arr[0] = arr[1];
+    arr[1] = temp;
 }
